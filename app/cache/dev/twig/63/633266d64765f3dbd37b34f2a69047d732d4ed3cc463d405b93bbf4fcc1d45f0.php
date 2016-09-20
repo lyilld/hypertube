9aa98152a9fa98fc3292e19f4fcf49ea813d56af
Ishak LEGHLAM <ileghlam@e3r11p22.42.fr>
2016-09-21 01:31:39 +0200

    push Ishak

---
+++ app/cache/dev/twig/63/633266d64765f3dbd37b34f2a69047d732d4ed3cc463d405b93bbf4fcc1d45f0.php
@@ -0,0 +1,40 @@
+<?php
+
+/* DiviAjaxLoginBundle:Javascript:erroraction.js.twig */
+class __TwigTemplate_150e4e9f34389df37af699e6aa8497b87affa58ff723b30547b59199866aa338 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_a89c9809b9ba34aaf1e73abf5bd319075573c981c73d7c306948ea0ba0e27848 = $this->env->getExtension("native_profiler");
+        $__internal_a89c9809b9ba34aaf1e73abf5bd319075573c981c73d7c306948ea0ba0e27848->enter($__internal_a89c9809b9ba34aaf1e73abf5bd319075573c981c73d7c306948ea0ba0e27848_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "DiviAjaxLoginBundle:Javascript:erroraction.js.twig"));
+
+        // line 1
+        echo "alert(data.error);
+";
+        
+        $__internal_a89c9809b9ba34aaf1e73abf5bd319075573c981c73d7c306948ea0ba0e27848->leave($__internal_a89c9809b9ba34aaf1e73abf5bd319075573c981c73d7c306948ea0ba0e27848_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "DiviAjaxLoginBundle:Javascript:erroraction.js.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+}
+/* alert(data.error);*/
+/* */
---
+++ src/UserBundle/EventListener/RegistrationConfirmListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\UserEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Redirection aprés enregistrement d'un utilisateur
+ */
+class RegistrationConfirmListener implements EventSubscriberInterface
+{
+  private $router;
+
+  public function __construct(UrlGeneratorInterface $router)
+   {
+       $this->router = $router;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public static function getSubscribedEvents()
+   {
+       return array(
+               FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationConfirm'
+       );
+   }
+
+   public function onRegistrationConfirm(\FOS\UserBundle\Event\FormEvent $event)
+   {
+      //$url = $this->router->generate('Video');
+
+      $event->setResponse(new RedirectResponse("http://symfony.com/doc/current/reference/forms/types/file.html#label-format"));
+   }
+}
+
+ ?>
